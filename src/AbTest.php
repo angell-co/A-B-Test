04b61582d97a57d2f124df44d7a4aa9b0086643f
Josh Angell <josh@angell.io>
2020-09-17 16:24:38 +0100

    POC using drafts and cookies!

--- src/AbTest.php
+++ src/AbTest.php
@@ -13,7 +13,18 @@ namespace angellco\abtest;
 use Craft;
 use craft\base\Plugin;
 
+use craft\db\Query;
+use craft\db\Table;
+use craft\elements\db\ElementQuery;
+use craft\elements\Entry;
+use craft\events\PopulateElementEvent;
+use craft\events\TemplateEvent;
+use craft\helpers\ConfigHelper;
+use craft\web\Application;
+use craft\web\View;
 use yii\base\Event;
+use yii\db\Exception as DbException;
+use yii\web\Cookie;
 
 /**
  * Class AbTest
@@ -32,6 +43,7 @@ class AbTest extends Plugin
      * @var AbTest
      */
     public static $plugin;
+    public static $cookie;
 
     // Public Properties
     // =========================================================================
@@ -62,13 +74,66 @@ class AbTest extends Plugin
         parent::init();
         self::$plugin = $this;
 
-        Craft::info(
-            Craft::t(
-                'ab-test',
-                '{name} plugin loaded',
-                ['name' => $this->name]
-            ),
-            __METHOD__
+        $request = Craft::$app->getRequest();
+        $response = Craft::$app->getResponse();
+
+        Event::on(Application::class, Application::EVENT_INIT,
+            function() use($request, $response) {
+                if ($response->getIsOk() && $request->getIsSiteRequest()) {
+                    $cookie = $request->getCookies()->get('abtest_1');
+                    if (!$cookie) {
+                        $cookie = new Cookie([
+                            'name' => 'abtest_1'
+                        ]);
+
+                        // Decide if control or not
+                        if (rand(0, 1) === 0) {
+                            $cookie->value = 'control';
+                        } else {
+                            $cookie->value = 'test';
+                        }
+
+                        $response->getCookies()->add($cookie);
+                    }
+                }
+            }
+        );
+
+        Event::on(ElementQuery::class, ElementQuery::EVENT_AFTER_POPULATE_ELEMENT,
+            function(PopulateElementEvent $event) use($request, $response) {
+
+                if ($response->getIsOk() && $request->getIsSiteRequest() && $event->element !== null && is_a($event->element, Entry::class)) {
+
+                    /** @var Entry $entry */
+                    $entry = $event->element;
+
+                    // Dumb check if its a draft ID so we don’t get a loop due to this event handler firing again when
+                    // we populate the draft lower down - refactor obvs
+                    if (!$entry->draftId) {
+
+                        $cookie = $request->getCookies()->get('abtest_1');
+
+                        if (!$cookie) {
+                            $cookie = $response->getCookies()->get('abtest_1');
+                        }
+
+                        if ($cookie && $cookie->value === 'test') {
+                            // Get draft IDs
+                            $query = Entry::find()
+                                ->draftOf($entry)
+                                ->siteId($entry->siteId)
+                                ->anyStatus()
+                                ->orderBy(['dateUpdated' => SORT_DESC])
+                                ->limit(1);
+                            $draftIds = $query->ids();
+                            if ($draftIds) {
+                                $selectedEntry = Craft::$app->getElements()->getElementById($draftIds[0]);
+                                $event->element = $selectedEntry;
+                            }
+                        }
+                    }
+                }
+            }
         );
     }
 
