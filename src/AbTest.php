b9e2ecdbcfd01d487f661ccafa42c9c64867083d
Josh Angell <josh@angell.io>
2020-09-29 16:05:53 +0100

    Basic interface for managing the selected drafts

--- src/AbTest.php
+++ src/AbTest.php
@@ -135,6 +135,38 @@ class AbTest extends Plugin
                 }
             }
         );
+
+        // Entries sidebar
+        if ($request->getIsCpRequest() && !$request->getIsConsoleRequest()) {
+            Craft::$app->getView()->hook('cp.entries.edit.details', function (&$context) {
+                $html = '';
+
+                /** @var  $entry Entry */
+                $entry = $context['entry'];
+                if ($entry !== null) {
+
+                    $drafts = Entry::find()
+                        ->draftOf($entry)
+                        ->siteId($entry->siteId)
+                        ->anyStatus()
+                        ->orderBy(['dateUpdated' => SORT_DESC])
+                        ->limit(null)
+                        ->all();
+
+                    $n = count($drafts);
+
+                    if ($drafts) {
+                        $html .= Craft::$app->view->renderTemplate('ab-test/entry-sidebar', [
+                            'drafts' => $drafts
+                        ]);
+                    }
+                }
+
+                return $html;
+            });
+        }
+
+
     }
 
 }
